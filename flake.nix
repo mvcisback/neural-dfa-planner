47e5c458b8a88c907708065bb81c88e34a718c10
Marcell Vazquez-Chanlatte <mvc@linux.com>
2024-04-07 10:54:50 -0700

    chore: Add more dependencies to flake.

--- flake.nix
+++ flake.nix
@@ -18,7 +18,65 @@
   } // inputs.utils.lib.eachSystem [
     "x86_64-linux"
   ] (system:
-    let pkgs = import nixpkgs {
+    let 
+	bitarray = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "bitarray";
+	  version = "2.9.2";
+	  format = "setuptools";
+	  src = pkgs.python3Packages.fetchPypi rec {
+            inherit pname version format;
+	    sha256 = "a8f286a51a32323715d77755ed959f94bef13972e9a2fe71b609e40e6d27957e";
+	  };
+	  nativeBuildInputs = [
+	    pkgs.python3
+            pkgs.buildPackages.python3Packages.cffi  # Example dependency for CFFI-based extensions
+            pkgs.buildPackages.python3Packages.setuptools
+            pkgs.buildPackages.python3Packages.setuptools-scm
+	    pkgs.buildPackages.python3Packages.wheel
+	  ];
+	  setupPyBuildFlags = [ "--inplace" ];
+	};
+        dfa = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "dfa";
+          version = "4.6.3";
+          format = "wheel";
+          src = pkgs.python3Packages.fetchPypi rec {
+            inherit pname version format;
+            sha256 = "b4d511f73eb1588a391cc4a032362c836053963a17aa4bc58b40c59a39ea639a";
+            dist = python;
+            python = "py3";
+          };
+          propagatedBuildInputs = [
+            pkgs.python311Packages.attrs
+            pkgs.python311Packages.funcy
+            pkgs.python311Packages.pydot
+            pkgs.python311Packages.bidict
+            bitarray
+          ];
+        };
+
+        py-aiger = pkgs.python3Packages.buildPythonPackage rec {
+          pname = "py_aiger";
+          version = "6.2.3";
+          format = "wheel";
+          src = pkgs.python3Packages.fetchPypi rec {
+            inherit pname version format;
+            sha256 = "837bcd9f49b5a945e5877126f3238e08731d32d545fab05b2146c776dca9c45c";
+            dist = python;
+            python = "py3";
+          };
+          propagatedBuildInputs = [
+            pkgs.python311Packages.attrs
+            pkgs.python311Packages.funcy
+            pkgs.python311Packages.pydot
+            pkgs.python311Packages.pyrsistent
+            pkgs.python311Packages.sortedcontainers
+            pkgs.python311Packages.bidict
+          ];
+        };
+
+
+        pkgs = import nixpkgs {
           inherit system;
           config = {
             allowUnfree = true;
@@ -31,11 +89,14 @@
     in {
       devShells.default = let
         python-env = pkgs.python3.withPackages (pyPkgs: with pyPkgs; [
+          equinox
+          jax
+          jaxlib-bin
           numpy
           pandas
-          jaxlib-bin
-          jax
-          equinox
+          py-aiger
+	  pkgs.python3Packages.pip
+	  dfa
         ]);
 
         name = "jax-equinox-basics";
@@ -44,12 +105,9 @@
 
         packages = [
           python-env
-	  pkgs.poetry
+          pkgs.python311Packages.flit
+          pkgs.python311Packages.ptpython
         ];
-
-        shellHooks = let pythonIcon = "f3e2"; in ''
-          export PS1="$(echo -e '\u${pythonIcon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-        '';
       };
     });
 }
